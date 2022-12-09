4334cc04c1e21f4e844f41b438a57033bf8894fc
Alina Hutan <92319150+HTNAlina@users.noreply.github.com>
2022-12-09 14:41:28 +0200

    Update LogIn

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -9,30 +9,92 @@ const LogIn = () => {
     navigate("/admin-home");
   }, [navigate]);
 
-  const onHouse1ImageClick = useCallback(() => {
+  const onHouseImageClick = useCallback(() => {
     navigate("/");
   }, [navigate]);
 
+  const onButonNoutatiClick = useCallback(() => {
+    navigate("/news");
+  }, [navigate]);
+
+  const onButonLotClick = useCallback(() => {
+    navigate("/lot");
+  }, [navigate]);
+
+  const onButonClasamentClick = useCallback(() => {
+    window.open("https://competitii.frvolei.eu/a1-masculin-sezon-2021-2022/");
+  }, []);
+
+  const onButonCalendarClick = useCallback(() => {
+    navigate("/calendar");
+  }, [navigate]);
+
+  const onButonDetaliiClick = useCallback(() => {
+    navigate("/details");
+  }, [navigate]);
+
+  const onButonJuvenilClick = useCallback(() => {
+    navigate("/volei-juvenil");
+  }, [navigate]);
+
+  const onButonAccountClick = useCallback(() => {
+    navigate("/login");
+  }, [navigate]);
+
   return (
-    <div className="log-in-div">
-      <div className="nume-div2">
-        <div className="nume-div3">Nume</div>
+    <><div className="log-in-div">
+      
+      <div className="nouati" onClick={onButonNoutatiClick}>
+        <img className="rectangle-n" alt="" src="../rectangle-1.svg" />
+        <div className="nouti-n">NOUTĂȚI</div>
       </div>
-    
+      <div className="lot" onClick={onButonLotClick}>
+        <div className="lot-l">LOT</div>
+        <img className="rectangle-l" alt="" src="../rectangle-5.svg" />
+      </div>
+      <button
+        className="buton-clasament-button"
+        onClick={onButonClasamentClick}
+      >
+        <div className="clasament-c">CLASAMENT</div>
+        <img className="rectangle-c" alt="" src="../rectangle-4.svg" />
+      </button>
+      <div
+        className="buton-calendar-div"
+        onClick={onButonCalendarClick}
+      >
+        <div className="calendar-c">CALENDAR</div>
+        <img className="rectangle-ca" alt="" src="../rectangle-3.svg" />
+      </div>
+      <div className="buton-detalii-div" onClick={onButonDetaliiClick}>
+        <div className="detalii-d">DETALII</div>
+        <img className="rectangle-d" alt="" src="../rectangle-2.svg" />
+      </div>
+      <div className="buton-volei-juvenil" onClick={onButonJuvenilClick}>
+        <img className="rectangle-v" alt="" src="../rectangle-31.svg" />
+        <div className="volei-juvenil-v">VOLEI JUVENIL</div>
+      </div>
+      <div className="buton-account-div" onClick={onButonAccountClick}>
+        <div className="rectangle-a" />
+        <img className="account-1-a" alt="" src="../account-1@2x.png" />
+      </div>
+
       <div className="autentificare-div">AUTENTIFICARE</div>
 
-      <input id="cod" type="text" className="cod-acces-div"placeholder="Cod acces"></input>
+      <input id="cod" type="text" className="cod-acces-div" placeholder="Cod acces"></input>
       <input id="nume" type="text" className="rectangle-select" placeholder="Nume complet"></input>
-      <button id="butonautentificare"className="autentificare-div1" onClick={onRectangleClick} >Autentificare</button>
-      
+      <button id="butonautentificare" className="autentificare-div1" onClick={onRectangleClick}>Autentificare</button>
+
       <img className="imagine-icon1" alt="" src="../imagine1@2x.png" />
-      <img
-        className="house-1-icon9"
-        alt=""
-        src="../house-19@2x.png"
-        onClick={onHouse1ImageClick}
-      />
-    </div>
+      
+    </div><div className="logo-div" onClick={onHouseImageClick}>
+        <img className="logo-csm-1" alt="" src="../logo-csm-1@2x.png" />
+        <div className="volei-csm-suceava">
+          <p className="volei-csm-p">VOLEI C.S.M</p>
+          <p className="suceava-p">SUCEAVA</p>
+        </div>
+      </div></>
+      
   );
 };
 
