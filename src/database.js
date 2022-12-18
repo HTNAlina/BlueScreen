46ab83435637e70ded5a4fea878b1feccb73289f
BotezatuAdrianGabriel <92536516+BotezatuAdrianGabriel@users.noreply.github.com>
2022-12-18 22:04:32 +0200

    update database

--- src/database.js
+++ src/database.js
@@ -1,11 +1,44 @@
+// const {createPool} =require('mysql')
 
-const {creatPool} =require('mysql')
-const pool = creatPool({
-  host: "localhost",
-  user: "root",
-  password: "AndreiSV123.",
-  conectionimit: 10
-})
-pool.query("SELECT * FROM poriect_ip.login;",(eer,res)=>{
+// const pool = createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "poriect",
+//   password: "AndreiSV123.",
+//   conectionimit: 10
+
+// })
+// pool.query('SELECT * FROM poriect_ip.login',(eer,res)=>{
+//   return console.log(res)
+// })
+
+// const {createPool} = require('mysql');
+
+//     const pool = createPool({
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'AndreiSV123.',
+//         port: 3306,          //port mysql
+//         database: 'ana.andrei',
+//         multipleStatements: 'true'  //false by default
+
+//     })
+// pool.query('SELECT * FROM ana.andrei;',(eer,res)=>{
+//   return console.log(res)
+// })
+const {createPool} = require("mysql");
+
+    const pool = createPool({
+        host: "localhost",
+        user: "root",
+        password: "AndreiSV123.",
+        port: 3306,          //port mysql
+        database: "ana.andrei",
+        multipleStatements: "true"  //false by default
+
+    })
+pool.query("SELECT * FROM ana.andrei;",(eer,res)=>{
   return console.log(res)
 })
+
+    
