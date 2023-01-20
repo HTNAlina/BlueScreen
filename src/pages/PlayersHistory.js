c10eaf75dc03261e2cb120697705c47f125fbed9
IulianB01 <iulian.bucatariu@student.usv.ro>
2023-01-20 18:25:46 +0200

    Update PlayersHistory

--- src/pages/PlayersHistory.js
+++ src/pages/PlayersHistory.js
@@ -8,9 +8,84 @@ const PlayersHistory = () => {
   const onHouse1ImageClick = useCallback(() => {
     navigate("/");
   }, [navigate]);
+  const onContainer1Click = useCallback(() => {
+    navigate("/players-history");
+  }, [navigate]);
+
+  const onHouseImageClick = useCallback(() => {
+    navigate("/");
+  }, [navigate]);
+  const onButonAccountClick = useCallback(() => {
+    navigate("/login");
+  }, [navigate]);
+
+  const onButonNoutatiClick = useCallback(() => {
+    navigate("/news");
+  }, [navigate]);
+
+  const onButonLotClick = useCallback(() => {
+    navigate("/lot");
+  }, [navigate]);
+
+  const onButonClasamentClick = useCallback(() => {
+    window.open("https://competitii.frvolei.eu/a1-masculin-sezon-2021-2022/");
+  }, []);
+
+  const onButonCalendarClick = useCallback(() => {
+    navigate("/calendar");
+  }, [navigate]);
+
+  const onButonDetaliiClick = useCallback(() => {
+    navigate("/details");
+  }, [navigate]);
+
+  const onButonJuvenilClick = useCallback(() => {
+    navigate("/volei-juvenil");
+  }, [navigate]); 
 
   return (
     <div className="players-history-div">
+       <div className="logo-div" onClick={onHouseImageClick}>
+        <img className="logo-csm-1" alt="" src="../logo-csm-1@2x.png" />
+        <div className="volei-csm-suceava">
+          <p className="volei-csm-p">VOLEI C.S.M</p>
+          <p className="suceava-p">SUCEAVA</p>
+        </div>
+      </div>
+      <div className="buton-account-div" onClick={onButonAccountClick}>
+        <div className="rectangle-a" />
+        <img className="account-1-a" alt="" src="../account-1@2x.png" />
+      </div>
+      <div className="nouati" onClick={onButonNoutatiClick}>
+        <img className="rectangle-n" alt="" src="../rectangle-1.svg" />
+        <div className="nouti-n">NOUTĂȚI</div>
+      </div>
+      <div className="lot" onClick={onButonLotClick}>
+        <div className="lot-l">LOT</div>
+        <img className="rectangle-l" alt="" src="../rectangle-5.svg" />
+      </div>
+      <div
+        className="buton-clasament-button"
+        onClick={onButonClasamentClick}
+      >
+        <div className="clasament-c">CLASAMENT</div>
+        <img className="rectangle-c" alt="" src="../rectangle-4.svg" />
+      </div>
+      <div
+        className="buton-calendar-div"
+        onClick={onButonCalendarClick}
+      >
+        <div className="calendar-c">CALENDAR</div>
+        <img className="rectangle-ca" alt="" src="../rectangle-3.svg" />
+      </div>
+      <div className="buton-detalii-div" onClick={onButonDetaliiClick}>
+        <div className="detalii-d">DETALII</div>
+        <img className="rectangle-d" alt="" src="../rectangle-2.svg" />
+      </div>
+      <div className="buton-volei-juvenil" onClick={onButonJuvenilClick}>
+        <img className="rectangle-v" alt="" src="../rectangle-31.svg" />
+        <div className="volei-juvenil-v">VOLEI JUVENIL</div>
+      </div>
       <div className="nume-jucator-div">
         <img className="line-icon10" alt="" src="../line-21.svg" />
         <div className="pop-darius-cristian">2 POP Darius Cristian</div>
@@ -67,12 +142,7 @@ const PlayersHistory = () => {
         <p className="nlime-183-p">&nbsp;</p>
         <p className="cs-unirea-dej">FRV 2021/22</p>
       </div>
-      <img
-        className="house-1-icon4"
-        alt=""
-        src="../house-14@2x.png"
-        onClick={onHouse1ImageClick}
-      />
+    
     </div>
   );
 };
